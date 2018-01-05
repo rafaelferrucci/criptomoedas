257dccde0358eaea8dfa00fac853bd7ed0057bb7
rafaelferrucci <35074720+rafaelferrucci@users.noreply.github.com>
2018-01-05 13:09:27 -0200

    Update twitterBot_criptomoedas.py

--- twitterBot_criptomoedas.py
+++ twitterBot_criptomoedas.py
@@ -9,14 +9,6 @@ import hmac
 import requests
 import time
 
-#coleta API bitcoinaverage
-
-secret_key = 'ODA4ZGY1ZGJkYWRiNDM2YzgxZjA2ZDExOTA4N2NmODM2ZmZlZjNhMmIxNGU0MWFjOThkOGY1OWJmOGE0ZmQxOQ'
-public_key = 'OTM2MzZkZTUyMWQ3NGUzYTg5MDZiYmQ5YmU3NjM5MWQ'
-timestamp = int(time.time())
-payload = '{}.{}'.format(timestamp, public_key)
-hex_hash = hmac.new(secret_key.encode(), msg=payload.encode(), digestmod=hashlib.sha256).hexdigest()
-signature = '{}.{}'.format(payload, hex_hash)
 
 
 #funções
@@ -35,23 +27,6 @@ def media_em_reais(siglamoeda):
     return media_moeda
 
 
-#BitCoin em Reais
-
-#media_bitcoin_brl_dia = media_em_reais('BTCBRL')
-       
-#Litecoin em Reais
-
-#media_litecoin_brl_dia = media_em_reais('LTCBRL')
-
-#BitCoin Cash em Reais
-
-#media_bitcoincash_brl_dia = media_em_reais('BCHBRL')
-
-#Ethereum Cash em Reais
-
-#media_ethereum_brl_dia = media_em_reais('ETHBRL')
-#frase_tweet = f'O valor médio das criptomoedas é:\n Bitcoin(BTC) R$ {media_bitcoin_brl_dia} \n Litecoin(LTC) R$ {media_litecoin_brl_dia} \n Bitcoin Cash(BCH) R$ {media_bitcoincash_brl_dia} \n Ethereum(ETH) R$ {media_ethereum_brl_dia} \n #bitcoin #litecoin #bitcashcoin #ethereum'
-
 import tweepy
 
 consumer_key = 'kRAmwa1Gs4ILHeL8vWwmvAgUD'
@@ -63,14 +38,23 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 while True:
-        media_bitcoin_brl_dia = media_em_reais('BTCBRL')
-        media_litecoin_brl_dia = media_em_reais('LTCBRL')
-        media_bitcoincash_brl_dia = media_em_reais('BCHBRL')
-        media_ethereum_brl_dia = media_em_reais('ETHBRL')
-        frase_tweet = f'O valor médio das criptomoedas é:\n Bitcoin(BTC) R$ {media_bitcoin_brl_dia} \n Litecoin(LTC) R$ {media_litecoin_brl_dia} \n Bitcoin Cash(BCH) R$ {media_bitcoincash_brl_dia} \n Ethereum(ETH) R$ {media_ethereum_brl_dia} \n #bitcoin #litecoin #bitcashcoin #ethereum'
-        api.update_status(frase_tweet)
-        time.sleep(900)
+        
+#coleta API bitcoinaverage
+
+    secret_key = 'ODA4ZGY1ZGJkYWRiNDM2YzgxZjA2ZDExOTA4N2NmODM2ZmZlZjNhMmIxNGU0MWFjOThkOGY1OWJmOGE0ZmQxOQ'
+    public_key = 'OTM2MzZkZTUyMWQ3NGUzYTg5MDZiYmQ5YmU3NjM5MWQ'
+    timestamp = int(time.time())
+    payload = '{}.{}'.format(timestamp, public_key)
+    hex_hash = hmac.new(secret_key.encode(), msg=payload.encode(), digestmod=hashlib.sha256).hexdigest()
+    signature = '{}.{}'.format(payload, hex_hash)
+
+    media_bitcoin_brl_dia = media_em_reais('BTCBRL')
+    media_litecoin_brl_dia = media_em_reais('LTCBRL')
+    media_bitcoincash_brl_dia = media_em_reais('BCHBRL')
+    media_ethereum_brl_dia = media_em_reais('ETHBRL')
+    frase_tweet = f'O valor médio das criptomoedas é:\n Bitcoin(BTC) R$ {media_bitcoin_brl_dia} \n Litecoin(LTC) R$ {media_litecoin_brl_dia} \n Bitcoin Cash(BCH) R$ {media_bitcoincash_brl_dia} \n Ethereum(ETH) R$ {media_ethereum_brl_dia} \n #bitcoin #litecoin #bitcashcoin #ethereum'
+    api.update_status(frase_tweet)
+    time.sleep(1800)
         
         
-#api.update_status(frase_tweet)
 
